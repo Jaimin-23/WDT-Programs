b7ee91ac2c1e2d8291a202c0311e9477cfc02a56
Jaimin-23 <jaiminleuva23@gmail.com>
2025-10-15 11:52:00 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Objects in Javascript</title>
+</head>
+<body>
+	<h3>DOM Manipulation</h3>
+	<div id="para1">Hello</div>
+	<div class="note">I am learning JS</div>
+	
+	<script>
+		
+	</script>
+</body>
+</html>
